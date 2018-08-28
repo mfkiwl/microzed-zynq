97078903870afa32626cf6a6b397cebb293e9210
Yi Qingliang <niqingliang2003@gmail.com>
2018-08-28 09:01:50 +0800

    script/fpcvt.sh: use tmp dir to avoid auto deletion by lib_convert.py
    Signed-off-by: Yi Qingliang <niqingliang2003@gmail.com>

--- scripts/fpcvt.sh
+++ scripts/fpcvt.sh
@@ -6,5 +6,8 @@ if [ "$1" == "" ]; then
 fi
 
 SRC_FILE="$1"
+TMP_DIR="`mktemp -d`"
 DST_DIR="`dirname $0`/../microzed.pretty"
-python2 /usr/share/doc/kicad/scripts/lib_convert.py ${SRC_FILE} ${DST_DIR}
+python2 /usr/share/doc/kicad/scripts/lib_convert.py ${SRC_FILE} ${TMP_DIR}
+mv ${TMP_DIR}/* ${DST_DIR}/
+rmdir ${TMP_DIR}
